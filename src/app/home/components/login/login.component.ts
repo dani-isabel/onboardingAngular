5d95847318c3cc0fe6a82442cb6e3b8cfc03d765
Daniela Isabel Bolivar Garcia <daniela.bolivar@globant.com>
2020-09-25 12:18:31 -0500

    save input value

--- src/app/home/components/login/login.component.ts
+++ src/app/home/components/login/login.component.ts
@@ -8,6 +8,16 @@ import { Component, OnInit, Input } from '@angular/core';
 export class LoginComponent implements OnInit {
   @Input() placeholder:string;
   @Input() send:string;
+  @Input() userName:string = '';
+
+  addUserName(value:string) {
+    if(value.length >= 3) {
+      this.userName = value;
+      alert(value);
+      console.log(this.userName);
+    } 
+    else {alert('You need enter a valid name');}
+  }
 
   constructor() { }
 
